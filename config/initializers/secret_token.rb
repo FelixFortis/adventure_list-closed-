056a541920cee3f5e2db565d9638ebc031aa9214
Adam Wenham <adamwenham64@gmail.com>
2013-02-16 12:45:08 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdventureList::Application.config.secret_token = 'd083c839817703008c6827e97ce47dc6fc7caf285d9e1f193bb1898f75328d5241b6a6e5ee1f1cd7f0b6cff2b7224f07bb82bf7de1ffbd0e1dadd355a6b390b0'
